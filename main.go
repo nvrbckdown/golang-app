b67e399ab805b6623639266687ce8bfc6a4db11d
Abdulkodir Yusupov <44323682+nvrbckdown@users.noreply.github.com>
2025-03-10 17:47:41 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,56 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-    "html/template"
-    "net/http"
+	"html/template"
+	"net/http"
 	"os"
+	"time"
 )
 
-type Kuber struct{
-	Title string
-    Pod string
-    Node string
-    Namespace string
+type Kuber struct {
+	Title     string
+	Pod       string
+	Node      string
+	Namespace string
 }
 
-type Env struct{
-    Title string
-    HTTP_PORT string
-    ENV string
+type Env struct {
+	Title     string
+	HTTP_PORT string
+	ENV       string
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-    http_port := os.Getenv("HTTP_PORT")
+	http_port := os.Getenv("HTTP_PORT")
 	env := os.Getenv("ENV")
-    
-    data := Env{
-        Title: "Env",
-        HTTP_PORT: http_port,
-        ENV: env,
-    }
-    tmpl, _ := template.ParseFiles("temp/layout.html")
-    tmpl.Execute(w, data)
+
+	data := Env{
+		Title:     "Env",
+		HTTP_PORT: http_port,
+		ENV:       env,
+	}
+	tmpl, _ := template.ParseFiles("temp/layout.html")
+	tmpl.Execute(w, data)
 }
 
 func Kubernetes(w http.ResponseWriter, r *http.Request) {
-    pod := os.Getenv("MY_POD_NAME")
+	// Added a 3-second delay before processing the request
+	time.Sleep(3 * time.Second)
+
+	pod := os.Getenv("MY_POD_NAME")
 	node := os.Getenv("MY_NODE_NAME")
-    ns := os.Getenv("MY_NAMESPACE")
-    data := Kuber{
-        Title: "k8s",
-        Pod: pod,
-        Node: node,
-        Namespace: ns,
-    }
-    tmpl, _ := template.ParseFiles("temp/kuber.html")
-    tmpl.Execute(w, data)
+	ns := os.Getenv("MY_NAMESPACE")
+	data := Kuber{
+		Title:     "k8s",
+		Pod:       pod,
+		Node:      node,
+		Namespace: ns,
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-    http.HandleFunc("/", HomePage)
-    http.HandleFunc("/k8s", Kubernetes)
- 
-    fmt.Println("Server is listening on port 8080")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/k8s", Kubernetes)
+
+	fmt.Println("Server is listening on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
